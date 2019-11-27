7268eb7043440a045e74ac49bbf1b2c5f4a82a0c
yogi <shah.yogi@tristonsoft.comshah.yogi@tristonsoft.com>
2019-11-27 14:57:15 +0530

    procfile

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ import os, sys
 if (os.environ.get("DEBUG", False) == "True"):
     app.config["DEBUG"] = True
 
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     print >> sys.stderr, "DEBUG Mode! Port: " + str(port) 
-    app.run(port=8000)
+    app.run()
 
     #app.run(debug=True)
 else:
-    app.run(port=8000)
+    app.run()
 
